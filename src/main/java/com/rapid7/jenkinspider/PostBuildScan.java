d7b9526079974e4e5ae5742af424cc2f57f9353f
Dinesh Reddy Parne <dineshparne@gmail.com>
2018-11-05 16:19:45 -0600

    Fix the file separator

--- src/main/java/com/rapid7/jenkinspider/PostBuildScan.java
+++ src/main/java/com/rapid7/jenkinspider/PostBuildScan.java
@@ -364,13 +364,13 @@ public class PostBuildScan extends Publisher implements SimpleBuildStep {
 
         /* Scan completed with either a 'Complete' or 'Stopped' status */
         log.println("Generating xml report to:" + filePath.getBaseName());
-        log.println("Generating :" + filePath.getParent() + "/" + filePath.getBaseName() + "/" + reportName + "_" +
+        log.println("Generating :" + filePath.getParent() + File.separator + filePath.getBaseName() + File.separator + reportName + "_" +
                 new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss").format(new Date()) + ".xml");
         String xmlFile = ReportManagement.getVulnerabilitiesSummaryXml(appSpiderEntUrl, appSpiderEntApiKey, scanId);
         log.println("Report :" + xmlFile);
 
         /* Saving the Report*/
-        SaveToFile(filePath.getParent() + "/" + filePath.getBaseName() + "/" + reportName + "_" +
+        SaveToFile(filePath.getParent() + File.separator + filePath.getBaseName() + File.separator + reportName + "_" +
                 new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss").format(new Date()) + ".xml", xmlFile);
         log.println("Generating report done.");
 
